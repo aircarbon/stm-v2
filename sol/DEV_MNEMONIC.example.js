d9bd9a8a3df94dbee57196c3c25f36676a3f2178
Antons Tesluks <anton.tesliuk@gmail.com>
2022-08-07 20:33:30 +0400

    Solidity formatting, partly removed futures

---
+++ sol/DEV_MNEMONIC.example.js
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-only - (c) AirCarbon Pte Ltd - see /LICENSE.md for Terms
+
+module.exports = {
+    // replace the value below with an actual mnemonics
+    MNEMONIC: 'juice juice juice juice juice juice juice juice juice juice juice juice',
+};
--- sol/contracts/StMaster/Migrations.sol
+++ sol/contracts/StMaster/Migrations.sol
@@ -3,39 +3,39 @@
 pragma solidity 0.8.5;
 
 /**
-  * @title Migrations
-  * @author Dominic Morris (7-of-9)
-  * @notice smart contract migrations contract
-  */
+ * @title Migrations
+ * @author Dominic Morris (7-of-9)
+ * @notice smart contract migrations contract
+ */
 contract Migrations {
-    address public owner;
-    uint256 public last_completed_migration;
+	address public owner;
+	uint256 public last_completed_migration;
 
-    /**
-     * @dev initialize the migrations contract
-     */
-    constructor() {
-        owner = msg.sender;
-    }
+	modifier restricted() {
+		if (msg.sender == owner) _;
+	}
 
-    modifier restricted() {
-        if (msg.sender == owner) _;
-    }
+	/**
+	 * @dev initialize the migrations contract
+	 */
+	constructor() {
+		owner = msg.sender;
+	}
 
-    /**
-     * @dev set the migration as completed
-     * @param completed completion attempt #
-     */
-    function setCompleted(uint256 completed) public restricted {
-        last_completed_migration = completed;
-    }
-    
-    /**
-     * @dev upgrade contract and intialize migration
-     * @param new_address new address for migration of old contract
-     */
-    function upgrade(address new_address) public restricted {
-        Migrations upgraded = Migrations(new_address);
-        upgraded.setCompleted(last_completed_migration);
-    }
+	/**
+	 * @dev set the migration as completed
+	 * @param completed completion attempt #
+	 */
+	function setCompleted(uint256 completed) public restricted {
+		last_completed_migration = completed;
+	}
+
+	/**
+	 * @dev upgrade contract and intialize migration
+	 * @param new_address new address for migration of old contract
+	 */
+	function upgrade(address new_address) public restricted {
+		Migrations upgraded = Migrations(new_address);
+		upgraded.setCompleted(last_completed_migration);
+	}
 }
