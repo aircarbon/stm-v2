785f3d6891152270dec8c2e279e2118d3234bab3
Antons Tesluks <anton.tesliuk@gmail.com>
2022-08-25 16:41:00 +0400

    Impl diamond. Impl CcyCollateralizable

---
+++ sol/contracts/facets/CcyCollateralizableFacet.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: AGPL-3.0-only - (c) AirCarbon Pte Ltd - see /LICENSE.md for Terms
+pragma solidity 0.8.5;
+
+import { StructLib } from "../libraries/StructLib.sol";
+import { CcyLib } from "../libraries/CcyLib.sol";
+import { LibMainStorage } from "../libraries/LibMainStorage.sol";
+import { OwnedLib } from "../libraries/OwnedLib.sol";
+
+contract CollateralizableFacet {
+
+	/**
+	 * @dev returns the current supporting currencies
+	 * @return ccyTypes
+	 * @param ccyTypes array of supporting currency types struct
+	 */
+	function getCcyTypes() external view returns (StructLib.GetCcyTypesReturn memory ccyTypes) {
+		return CcyLib.getCcyTypes(LibMainStorage.getStorage().ctd);
+	}
+
+	/**
+	 * @dev add supporting currency types
+	 * @param name name of the currency
+	 * @param unit unit of the currency
+	 * @param decimals level of precision of the currency
+	 */
+	function addCcyType(
+		string memory name,
+		string memory unit,
+		uint16 decimals
+	) public {
+		OwnedLib.onlyOwner();
+		OwnedLib.onlyWhenReadWrite();
+		LibMainStorage.MainStorage storage s = LibMainStorage.getStorage();
+		CcyLib.addCcyType(s.ld, s.ctd, name, unit, decimals);
+	}
+
+	/**
+	 * @dev fund or withdraw currency type collaterised tokens from a ledger owner address
+	 * @param direction 0: FUND<br/>1: WITHDRAW
+	 * @param ccyTypeId currency type identifier
+	 * @param amount amount to be funded or withdrawn
+	 * @param ledgerOwner account address to be funded or withdrawn from
+	 * @param desc supporting evidence like bank wire reference or comments
+	 */
+	function fundOrWithdraw(
+		StructLib.FundWithdrawType direction,
+		uint256 ccyTypeId,
+		int256 amount,
+		address ledgerOwner,
+		string calldata desc
+	) public 
+		// hasEntity(ledgerOwner) // TODO: check this
+	{	OwnedLib.onlyOwner();
+		OwnedLib.onlyWhenReadWrite();
+
+		LibMainStorage.MainStorage storage s = LibMainStorage.getStorage();
+		CcyLib.fundOrWithdraw(s.ld, s.ctd, direction, ccyTypeId, amount, ledgerOwner, desc);
+	}
+}
