383efb0cc2e878fdc5e60095a175a2983ad391c1
Antons Tesluks <anton.tesliuk@gmail.com>
2022-10-25 10:50:50 +0400

    Fixing 15 Test Funding

--- sol/test_disabled/15_Test_Funding.js
+++ sol/test_disabled/15_Test_Funding.js
@@ -2,25 +2,62 @@
 // Author: https://github.com/7-of-9
 
 // Re: CcyCollateralizable.sol => CcyLib.sol
-const BigNumber = require('big-number');
-const st = artifacts.require('StMaster');
+const st = artifacts.require('DiamondProxy');
+const StMasterFacet = artifacts.require('StMasterFacet');
+const StErc20Facet = artifacts.require('StErc20Facet');
+
+const StMintableFacet = artifacts.require('StMintableFacet');
+const CcyCollateralizableFacet = artifacts.require('CcyCollateralizableFacet');
+const StLedgerFacet = artifacts.require('StLedgerFacet');
+const StFeesFacet = artifacts.require('StFeesFacet');
+const StTransferableFacet = artifacts.require('StTransferableFacet');
+const OwnedFacet = artifacts.require('OwnedFacet');
+
 const truffleAssert = require('truffle-assertions');
 const CONST = require('../const.js');
-const setupHelper = require('../test/testSetupContract.js');
+const setupHelper = require('./testSetupContract.js');
 
-contract("StMaster", accounts => {
+contract("DiamondProxy", accounts => {
     var stm;
+    var stmStMasterFacet;
+    var stmStErc20Facet;
+    var stmStMintableFacet;
+    var stmCcyCollateralizableFacet;
+    var stmStLedgerFacet;
+    var stmStFeesFacet;
+    var stmStTransferableFacet;
+    var stmOwnedFacet;
 
     before(async function () {
         stm = await st.deployed();
-        if (await stm.getContractType() != CONST.contractType.COMMODITY) this.skip();
-        await stm.sealContract();
-        await setupHelper.setDefaults({ stm, accounts });
+        const addr = stm.address;
+
+        stmStMasterFacet = await StMasterFacet.at(addr);
+        stmStErc20Facet = await StErc20Facet.at(addr);
+        stmStMintableFacet = await StMintableFacet.at(addr);
+        stmCcyCollateralizableFacet = await CcyCollateralizableFacet.at(addr);
+        stmStLedgerFacet = await StLedgerFacet.at(addr);
+        stmStFeesFacet = await StFeesFacet.at(addr);
+        stmStTransferableFacet = await StTransferableFacet.at(addr);
+        stmOwnedFacet = await OwnedFacet.at(addr);
+
+        if (await stmStMasterFacet.getContractType() != CONST.contractType.COMMODITY) this.skip();
         if (!global.TaddrNdx) global.TaddrNdx = 0;
+
+        await stmStErc20Facet.whitelistMany(accounts.slice(global.TaddrNdx, global.TaddrNdx + 50));
+        await stmStMasterFacet.sealContract();
+        await setupHelper.setDefaults({ 
+            StErc20Facet: stmStErc20Facet, 
+            stmStMaster: stmStMasterFacet, 
+            stmStLedger: stmStLedgerFacet, 
+            stmCcyCollateralizable: stmCcyCollateralizableFacet, 
+            stmFees: stmStFeesFacet,
+            accounts });
     });
 
     beforeEach(async () => {
         global.TaddrNdx++;
+        await stmStErc20Facet.setAccountEntity({id: 1, addr: accounts[global.TaddrNdx]});
         if (CONST.logTestAccountUsage)
             console.log(`addrNdx: ${global.TaddrNdx} - contract @ ${stm.address} (owner: ${accounts[0]})`);
     });
@@ -66,9 +103,9 @@ contract("StMaster", accounts => {
     });
 
     it(`funding - should allow minting and funding on same ledger entry`, async () => {
-        await stm.mintSecTokenBatch(CONST.tokenType.TOK_T2, CONST.GT_CARBON, 1, accounts[global.TaddrNdx], CONST.nullFees, 0, [], [], { from: accounts[0] });
-        await stm.fundOrWithdraw(CONST.fundWithdrawType.FUND, CONST.ccyType.USD, CONST.thousandCcy_cents, accounts[global.TaddrNdx], 'TEST');
-        const ledgerEntryAfter = await stm.getLedgerEntry(accounts[global.TaddrNdx]);
+        await stmStMintableFacet.mintSecTokenBatch(CONST.tokenType.TOK_T2, CONST.GT_CARBON, 1, accounts[global.TaddrNdx], CONST.nullFees, 0, [], [], { from: accounts[0] });
+        await stmCcyCollateralizableFacet.fundOrWithdraw(CONST.fundWithdrawType.FUND, CONST.ccyType.USD, CONST.thousandCcy_cents, accounts[global.TaddrNdx], 'TEST');
+        const ledgerEntryAfter = await stmStLedgerFacet.getLedgerEntry(accounts[global.TaddrNdx]);
 
         assert(ledgerEntryAfter.tokens.length == 1, 'unexpected eeu count in ledger entry after minting & funding');
         assert(Number(ledgerEntryAfter.spot_sumQty) == Number(CONST.GT_CARBON), 'invalid kg sum in ledger entry after minting & funding');
@@ -77,7 +114,7 @@ contract("StMaster", accounts => {
 
     it(`funding - should not allow non-owner to fund a ledger entry`, async () => {
         try {
-            await stm.fundOrWithdraw(CONST.fundWithdrawType.FUND, CONST.ccyType.USD, 100, accounts[global.TaddrNdx], 'TEST', { from: accounts[10] });
+            await stmCcyCollateralizableFacet.fundOrWithdraw(CONST.fundWithdrawType.FUND, CONST.ccyType.USD, 100, accounts[global.TaddrNdx], 'TEST', { from: accounts[10] });
         } catch (ex) { 
             assert(ex.reason == 'Restricted', `unexpected: ${ex.reason}`);
             return;
@@ -87,7 +124,7 @@ contract("StMaster", accounts => {
 
     it(`funding - should not allow non-existent currency types`, async () => {
         try {
-            await stm.fundOrWithdraw(CONST.fundWithdrawType.FUND, 9999, 100, accounts[global.TaddrNdx], 'TEST', { from: accounts[0] });
+            await stmCcyCollateralizableFacet.fundOrWithdraw(CONST.fundWithdrawType.FUND, 9999, 100, accounts[global.TaddrNdx], 'TEST', { from: accounts[0] });
         } catch (ex) { 
             assert(ex.reason == 'Bad ccyTypeId', `unexpected: ${ex.reason}`);
             return;
@@ -97,7 +134,7 @@ contract("StMaster", accounts => {
 
     it(`funding - should not allow invalid amounts`, async () => {
         try {
-            await stm.fundOrWithdraw(CONST.fundWithdrawType.FUND, CONST.ccyType.USD, -1, accounts[global.TaddrNdx], 'TEST', { from: accounts[0] });
+            await stmCcyCollateralizableFacet.fundOrWithdraw(CONST.fundWithdrawType.FUND, CONST.ccyType.USD, -1, accounts[global.TaddrNdx], 'TEST', { from: accounts[0] });
         } catch (ex) { 
             assert(ex.reason == 'Bad amount', `unexpected: ${ex.reason}`);
             return;
@@ -107,26 +144,26 @@ contract("StMaster", accounts => {
 
     it(`funding - should not allow when contract is read only`, async () => {
         try {
-            await stm.setReadOnly(true, { from: accounts[0] });
-            await stm.fundOrWithdraw(CONST.fundWithdrawType.FUND, CONST.ccyType.USD, 100, accounts[global.TaddrNdx], 'TEST', { from: accounts[0] });
+            await stmOwnedFacet.setReadOnly(true, { from: accounts[0] });
+            await stmCcyCollateralizableFacet.fundOrWithdraw(CONST.fundWithdrawType.FUND, CONST.ccyType.USD, 100, accounts[global.TaddrNdx], 'TEST', { from: accounts[0] });
         } catch (ex) { 
             assert(ex.reason == 'Read-only', `unexpected: ${ex.reason}`);
-            await stm.setReadOnly(false, { from: accounts[0] });
+            await stmOwnedFacet.setReadOnly(false, { from: accounts[0] });
             return;
         }
-        await stm.setReadOnly(false, { from: accounts[0] });
+        await stmOwnedFacet.setReadOnly(false, { from: accounts[0] });
         assert.fail('expected contract exception');
     });
     
     async function fundLedger({ ccyTypeId, amount, receiver }) {
         var ledgerEntryBefore, ledgerEntryAfter;
 
-        ledgerEntryBefore = await stm.getLedgerEntry(receiver);
+        ledgerEntryBefore = await stmStLedgerFacet.getLedgerEntry(receiver);
         //const totalFundedBefore = await stm.getTotalCcyFunded.call(ccyTypeId);
         
         // fund
-        const fundTx = await stm.fundOrWithdraw(CONST.fundWithdrawType.FUND, ccyTypeId, amount, receiver, 'TEST', { from: accounts[0] });
-        ledgerEntryAfter = await stm.getLedgerEntry(receiver);
+        const fundTx = await stmCcyCollateralizableFacet.fundOrWithdraw(CONST.fundWithdrawType.FUND, ccyTypeId, amount, receiver, 'TEST', { from: accounts[0] });
+        ledgerEntryAfter = await stmStLedgerFacet.getLedgerEntry(receiver);
         truffleAssert.eventEmitted(fundTx, 'CcyFundedLedger', ev => {
             return ev.ccyTypeId == ccyTypeId
                 && ev.to == receiver
