2d900f72e43536b74be3a522b0f29805a39d12e9
Antons Tesluks <anton.tesliuk@gmail.com>
2022-11-03 17:30:05 +0400

    Fixing test for ccy and tok types

--- sol/test_disabled/12_Test_CcyTypes.js
+++ sol/test_disabled/12_Test_CcyTypes.js
@@ -2,36 +2,69 @@
 // Author: https://github.com/7-of-9
 
 // Re: CcyCollateralizable.sol => CcyLib.sol
-const st = artifacts.require('StMaster');
+const st = artifacts.require('DiamondProxy');
+const StMasterFacet = artifacts.require('StMasterFacet');
+const StErc20Facet = artifacts.require('StErc20Facet');
+
+const StMintableFacet = artifacts.require('StMintableFacet');
+const CcyCollateralizableFacet = artifacts.require('CcyCollateralizableFacet');
+const StLedgerFacet = artifacts.require('StLedgerFacet');
+const StFeesFacet = artifacts.require('StFeesFacet');
+const StTransferableFacet = artifacts.require('StTransferableFacet');
+const OwnedFacet = artifacts.require('OwnedFacet');
+
 const truffleAssert = require('truffle-assertions');
 const CONST = require('../const.js');
 const setupHelper = require('../test/testSetupContract.js');
 
-contract("StMaster", accounts => {
-    var stm;
+contract("DiamondProxy", accounts => {
+    let stm;
+    let stmStMasterFacet;
+    let stmStErc20Facet;
+    let stmCcyCollateralizableFacet;
+    let stmStLedgerFacet;
+    let stmStFeesFacet;
+    let stmOwnedFacet;
 
     var DEF_CCY_TYPE_COUNT;
 
     before(async function () {
         stm = await st.deployed();
-        const contractType = await stm.getContractType();
-        if (await stm.getContractType() != CONST.contractType.COMMODITY) this.skip();
+        const addr = stm.address;
 
-        await stm.sealContract();
-        await setupHelper.setDefaults({ stm, accounts });
-        DEF_CCY_TYPE_COUNT = contractType == CONST.contractType.COMMODITY ? 3 : 2;
-        
+        stmStMasterFacet = await StMasterFacet.at(addr);
+        stmStErc20Facet = await StErc20Facet.at(addr);
+        stmCcyCollateralizableFacet = await CcyCollateralizableFacet.at(addr);
+        stmStLedgerFacet = await StLedgerFacet.at(addr);
+        stmStFeesFacet = await StFeesFacet.at(addr);
+        stmOwnedFacet = await OwnedFacet.at(addr);
+
+        const contractType = await stmStMasterFacet.getContractType();
+        if (await contractType != CONST.contractType.COMMODITY) this.skip();
+
         if (!global.TaddrNdx) global.TaddrNdx = 0;
+        await stmStErc20Facet.whitelistMany(accounts.slice(global.TaddrNdx, global.TaddrNdx + 40));
+        await stmStMasterFacet.sealContract();
+
+        await setupHelper.setDefaults({ 
+            StErc20Facet: stmStErc20Facet, 
+            stmStMaster: stmStMasterFacet, 
+            stmStLedger: stmStLedgerFacet, 
+            stmCcyCollateralizable: stmCcyCollateralizableFacet, 
+            stmFees: stmStFeesFacet,
+            accounts});
+        DEF_CCY_TYPE_COUNT = contractType == CONST.contractType.COMMODITY ? 3 : 2;
     });
 
     beforeEach(async () => {
         global.TaddrNdx++;
+        await stmStErc20Facet.setAccountEntity({id: 1, addr: accounts[global.TaddrNdx]});
         if (CONST.logTestAccountUsage)
-            console.log(`addrNdx: ${global.TaddrNdx} - contract @ ${stm.address} (owner: ${accounts[0]})`);
+            console.log(`addrNdx: ${global.TaddrNdx} - contract @ ${addr} (owner: ${accounts[0]})`);
     });
 
     it(`ccy types - should have correct default (ID 1-based) values`, async () => {
-        const types = (await stm.getCcyTypes()).ccyTypes;
+        const types = (await stmCcyCollateralizableFacet.getCcyTypes()).ccyTypes;
         assert(types.length == DEF_CCY_TYPE_COUNT, 'unexpected default ccy type count');
 
         assert(types[0].name == 'USD', 'unexpected default ccy type name 1');
@@ -45,8 +78,8 @@ contract("StMaster", accounts => {
 
     it(`ccy types - should make visible newly added currency types in the ledger`, async () => {
         // add new ccy type
-        const addCcyTx = await stm.addCcyType('TEST_COIN', 'TEST_UNIT', 2);
-        const types = (await stm.getCcyTypes()).ccyTypes;
+        const addCcyTx = await stmCcyCollateralizableFacet.addCcyType('TEST_COIN', 'TEST_UNIT', 2);
+        const types = (await stmCcyCollateralizableFacet.getCcyTypes()).ccyTypes;
 
         const newTypeId = types.filter(p => p.name == 'TEST_COIN')[0].id;
         assert(types.filter(p => p.name == 'TEST_COIN')[0].id == DEF_CCY_TYPE_COUNT + 1, 'unexpected/missing new currency type id');
@@ -54,7 +87,7 @@ contract("StMaster", accounts => {
         truffleAssert.eventEmitted(addCcyTx, 'AddedCcyType', ev => ev.id == DEF_CCY_TYPE_COUNT + 1 && ev.name == 'TEST_COIN' && ev.unit == 'TEST_UNIT');
 
         // validate ledger entry (non-existent) has the new type
-        const ledgerEntryAfter = await stm.getLedgerEntry(accounts[global.TaddrNdx]);
+        const ledgerEntryAfter = await stmStLedgerFacet.getLedgerEntry(accounts[global.TaddrNdx]);
         assert(ledgerEntryAfter.ccys.some(p => p.ccyTypeId == newTypeId), 'missing new currency type id from ledger after minting');
         assert(ledgerEntryAfter.ccys.some(p => p.name == 'TEST_COIN'), 'missing/invalid new currency name from ledger after minting');
         assert(ledgerEntryAfter.ccys.some(p => p.unit == 'TEST_UNIT'), 'missing/invalid new currency unit from ledger after minting');
@@ -62,20 +95,20 @@ contract("StMaster", accounts => {
 
     it(`ccy types - should allow funding of newly added currency types`, async () => {
         // add new ccy type
-        await stm.addCcyType('TEST_COIN2', 'TEST_UNIT', 42);
-        const types = (await stm.getCcyTypes()).ccyTypes;
+        await stmCcyCollateralizableFacet.addCcyType('TEST_COIN2', 'TEST_UNIT', 42);
+        const types = (await stmCcyCollateralizableFacet.getCcyTypes()).ccyTypes;
         const newTypeId = types.filter(p => p.name == 'TEST_COIN2')[0].id;
         assert(types.filter(p => p.name == 'TEST_COIN2')[0].decimals == 42, 'unexpected # decimal places on new currency type');
 
         // fund new ccy type & validate
-        await stm.fundOrWithdraw(CONST.fundWithdrawType.FUND, newTypeId, 424242, accounts[global.TaddrNdx], 'TEST');
-        ledgerEntryAfter = await stm.getLedgerEntry(accounts[global.TaddrNdx]);
+        await stmCcyCollateralizableFacet.fundOrWithdraw(CONST.fundWithdrawType.FUND, newTypeId, 424242, accounts[global.TaddrNdx], 'TEST');
+        ledgerEntryAfter = await stmStLedgerFacet.getLedgerEntry(accounts[global.TaddrNdx]);
         assert(ledgerEntryAfter.ccys.find(p => p.ccyTypeId == newTypeId).balance == 424242, 'unexpected ledger balance of new currency type after funding');
     });
 
     it(`ccy types - should not allow non-owner to add a currency type`, async () => {
         try {
-            await stm.addCcyType('NEW_TYPE_ID_3', 'test_unit', 2, { from: accounts[10] });
+            await stmCcyCollateralizableFacet.addCcyType('NEW_TYPE_ID_3', 'test_unit', 2, { from: accounts[10] });
         } catch (ex) { 
             assert(ex.reason == 'Restricted', `unexpected: ${ex.reason}`);
             return;
@@ -85,7 +118,7 @@ contract("StMaster", accounts => {
 
     it(`ccy types - should not allow adding an existing currency type name`, async () => {
         try {
-            await stm.addCcyType('USD', 'test_unit', 2, { from: accounts[0] });
+            await stmCcyCollateralizableFacet.addCcyType('USD', 'test_unit', 2, { from: accounts[0] });
         } catch (ex) { 
             assert(ex.reason == 'Currency type name already exists', `unexpected: ${ex.reason}`);
             return; 
@@ -95,14 +128,14 @@ contract("StMaster", accounts => {
 
     it(`ccy types - should not allow adding a currency type when contract is read only`, async () => {
         try {
-            await stm.setReadOnly(true, { from: accounts[0] }); 
-            await stm.addCcyType('NEW_TYPE_ID_4', 'test_unit', 2, { from: accounts[0] });
-        } catch (ex) { 
+            await stmOwnedFacet.setReadOnly(true, { from: accounts[0] }); 
+            await stmCcyCollateralizableFacet.addCcyType('NEW_TYPE_ID_4', 'test_unit', 2, { from: accounts[0] });
+        } catch (ex) {
             assert(ex.reason == 'Read-only', `unexpected: ${ex.reason}`);
-            await stm.setReadOnly(false, { from: accounts[0] });
+            await stmOwnedFacet.setReadOnly(false, { from: accounts[0] });
             return;
         }
-        await stm.setReadOnly(false, { from: accounts[0] });
+        await stmOwnedFacet.setReadOnly(false, { from: accounts[0] });
         assert.fail('expected contract exception');
     });
 });
--- sol/test_disabled/13_Test_TokTypes.js
+++ sol/test_disabled/13_Test_TokTypes.js
@@ -2,53 +2,88 @@
 // Author: https://github.com/7-of-9
 
 // Re: StLedger.sol => TokenLib.sol
-const st = artifacts.require('StMaster');
+const st = artifacts.require('DiamondProxy');
+const StMasterFacet = artifacts.require('StMasterFacet');
+const StErc20Facet = artifacts.require('StErc20Facet');
+
+const StMintableFacet = artifacts.require('StMintableFacet');
+const CcyCollateralizableFacet = artifacts.require('CcyCollateralizableFacet');
+const StLedgerFacet = artifacts.require('StLedgerFacet');
+const StFeesFacet = artifacts.require('StFeesFacet');
+const OwnedFacet = artifacts.require('OwnedFacet');
+
 const truffleAssert = require('truffle-assertions');
 const BN = require('bn.js');
 const CONST = require('../const.js');
 const setupHelper = require('../test/testSetupContract.js');
 
-contract("StMaster", accounts => {
-    var stm;
+contract("DiamondProxy", accounts => {
+    let stm;
+    let stmStMasterFacet;
+    let stmStErc20Facet;
+    let stmCcyCollateralizableFacet;
+    let stmStLedgerFacet;
+    let stmStFeesFacet;
+    let stmOwnedFacet;
+    let stmStMintableFacet;
 
     const DEF_SEC_TOKEN_COUNT = 3;
 
     before(async function () {
         stm = await st.deployed();
-        const contractType = await stm.getContractType();
-        if (await stm.getContractType() != CONST.contractType.COMMODITY) this.skip();
-        await stm.sealContract();
-        await setupHelper.setDefaults({ stm, accounts });
+        const addr = stm.address;
 
+        stmStMasterFacet = await StMasterFacet.at(addr);
+        stmStErc20Facet = await StErc20Facet.at(addr);
+        stmCcyCollateralizableFacet = await CcyCollateralizableFacet.at(addr);
+        stmStLedgerFacet = await StLedgerFacet.at(addr);
+        stmStFeesFacet = await StFeesFacet.at(addr);
+        stmOwnedFacet = await OwnedFacet.at(addr);
+        stmStMintableFacet = await StMintableFacet.at(addr);
+
+        const contractType = await stmStMasterFacet.getContractType();
+        if (await contractType != CONST.contractType.COMMODITY) this.skip();
+        
         if (!global.TaddrNdx) global.TaddrNdx = 0;
+        await stmStErc20Facet.whitelistMany(accounts.slice(global.TaddrNdx, global.TaddrNdx + 40));
+        
+        await stmStMasterFacet.sealContract();
+        await setupHelper.setDefaults({ 
+            StErc20Facet: stmStErc20Facet, 
+            stmStMaster: stmStMasterFacet, 
+            stmStLedger: stmStLedgerFacet, 
+            stmCcyCollateralizable: stmCcyCollateralizableFacet, 
+            stmFees: stmStFeesFacet,
+            accounts});
     });
 
     beforeEach(async () => {
         global.TaddrNdx++;
+        await stmStErc20Facet.setAccountEntity({id: 1, addr: accounts[global.TaddrNdx]});
         if (CONST.logTestAccountUsage)
-            console.log(`addrNdx: ${global.TaddrNdx} - contract @ ${stm.address} (owner: ${accounts[0]})`);
+            console.log(`addrNdx: ${global.TaddrNdx} - contract @ ${addr} (owner: ${accounts[0]})`);
     });
 
     it(`token types - should have correct default values`, async () => {
-        const types = (await stm.getSecTokenTypes()).tokenTypes;
+        const types = (await stmStLedgerFacet.getSecTokenTypes()).tokenTypes;
         console.log({types: types});
         assert(types.length == DEF_SEC_TOKEN_COUNT, 'unexpected default type count');
 
-        assert(types[0].name == 'CET', `unexpected default type name 1`);
+        assert(types[0].name == 'AirCarbon CORSIA Token', `unexpected default type name 1`);
         assert(types[0].id == 1, 'unexpected default type id 1');
 
-        assert(types[1].name== 'ANT', `unexpected default type name 2`);
+        assert(types[1].name== 'AirCarbon Nature Token', `unexpected default type name 2`);
         assert(types[1].id == 2, 'unexpected default type id 2');
 
-        assert(types[2].name== 'GPT', `unexpected default type name 3`);
+        assert(types[2].name== 'AirCarbon Premium Token', `unexpected default type name 3`);
         assert(types[2].id == 3, 'unexpected default type id 3');
     });
 
     it(`token types - should be able to use newly added ST types`, async () => {
         // add new ST type
-        const addSecTokenTx = await stm.addSecTokenType('NEW_TYPE_NAME_2', CONST.settlementType.SPOT, CONST.nullFutureArgs, CONST.nullAddr);
+        const addSecTokenTx = await stmStLedgerFacet.addSecTokenType('NEW_TYPE_NAME_2', CONST.settlementType.SPOT, CONST.nullFutureArgs, CONST.nullAddr);
 
-        const types = (await stm.getSecTokenTypes()).tokenTypes;
+        const types = (await stmStLedgerFacet.getSecTokenTypes()).tokenTypes;
         assert(types.filter(p => p.name == 'NEW_TYPE_NAME_2')[0].id == DEF_SEC_TOKEN_COUNT + 1, 'unexpected/missing new eeu type (2)');
         truffleAssert.eventEmitted(addSecTokenTx, 'AddedSecTokenType', ev => ev.id == DEF_SEC_TOKEN_COUNT + 1 && ev.name == 'NEW_TYPE_NAME_2');
 
@@ -56,32 +91,32 @@ contract("StMaster", accounts => {
         const newTypeId = types.filter(p => p.name == 'NEW_TYPE_NAME_2')[0].id;
 
         // batch count before
-        const batchCountBefore = (await stm.getSecTokenBatch_MaxId.call()).toNumber(); 
+        const batchCountBefore = (await stmStLedgerFacet.getSecTokenBatch_MaxId.call()).toNumber(); 
         //console.log(`batchCountBefore`, batchCountBefore);
         
         // mint new ST type: 2 batches 
         for (var i=0 ; i < 2 ; i++) {
-            await stm.mintSecTokenBatch(newTypeId, CONST.GT_CARBON * 100, 1, accounts[global.TaddrNdx], CONST.nullFees, 0, [], [], { from: accounts[0] });
+            await stmStMintableFacet.mintSecTokenBatch(newTypeId, CONST.GT_CARBON * 100, 1, accounts[global.TaddrNdx], CONST.nullFees, 0, [], [], { from: accounts[0] });
         }
-        const batchCountAfter_Mint1 = (await stm.getSecTokenBatch_MaxId.call()).toNumber(); 
+        const batchCountAfter_Mint1 = (await stmStLedgerFacet.getSecTokenBatch_MaxId.call()).toNumber(); 
         assert(batchCountAfter_Mint1 == batchCountBefore + 2, `unexpected max batch id ${batchCountAfter_Mint1} after minting (1)`);
 
         // mint default ST type: 4 batches 
         for (var i=0 ; i < 4 ; i++) {
-            await stm.mintSecTokenBatch(CONST.tokenType.TOK_T1, CONST.GT_CARBON * 100, 1, accounts[global.TaddrNdx], CONST.nullFees, 0, [], [], { from: accounts[0] });
+            await stmStMintableFacet.mintSecTokenBatch(CONST.tokenType.TOK_T1, CONST.GT_CARBON * 100, 1, accounts[global.TaddrNdx], CONST.nullFees, 0, [], [], { from: accounts[0] });
         }
-        const batchCountAfter_Mint2 = (await stm.getSecTokenBatch_MaxId.call()).toNumber();
+        const batchCountAfter_Mint2 = (await stmStLedgerFacet.getSecTokenBatch_MaxId.call()).toNumber();
         assert(batchCountAfter_Mint2 == batchCountBefore + 6, `unexpected max batch id ${batchCountAfter_Mint2} after minting (2)`);
 
         // validate ledger: 6 vSTs, 2 of new type
-        const ledgerEntryAfter = await stm.getLedgerEntry(accounts[global.TaddrNdx]);
+        const ledgerEntryAfter = await stmStLedgerFacet.getLedgerEntry(accounts[global.TaddrNdx]);
         assert(ledgerEntryAfter.tokens.length == 6, 'unexpected eeu count in ledger');
         assert(ledgerEntryAfter.tokens.filter(p => p.tokTypeId == newTypeId).length == 2, 'unexpected new eeu type in ledger');
     });
 
     it(`token types - should not allow non-owner to add an ST type`, async () => {
         try {
-            await stm.addSecTokenType(`NEW_TYPE_NAME_${new Date().getTime()}`, CONST.settlementType.SPOT, CONST.nullFutureArgs, CONST.nullAddr, { from: accounts[10] });
+            await stmStLedgerFacet.addSecTokenType(`NEW_TYPE_NAME_${new Date().getTime()}`, CONST.settlementType.SPOT, CONST.nullFutureArgs, CONST.nullAddr, { from: accounts[10] });
         } catch (ex) { 
             assert(ex.reason == 'Restricted', `unexpected: ${ex.reason}`);
             return; 
@@ -91,8 +126,8 @@ contract("StMaster", accounts => {
 
     it(`token types - should not allow adding an existing ST type name`, async () => {
         try {
-            const types = (await stm.getSecTokenTypes()).tokenTypes;
-            await stm.addSecTokenType(types[0].name, CONST.settlementType.SPOT, CONST.nullFutureArgs, CONST.nullAddr, { from: accounts[0] });
+            const types = (await stmStLedgerFacet.getSecTokenTypes()).tokenTypes;
+            await stmStLedgerFacet.addSecTokenType(types[0].name, CONST.settlementType.SPOT, CONST.nullFutureArgs, CONST.nullAddr, { from: accounts[0] });
         } catch (ex) { 
             assert(ex.reason == 'Duplicate name', `unexpected: ${ex.reason}`);
             return; 
@@ -102,20 +137,20 @@ contract("StMaster", accounts => {
 
     it(`token types - should not allow adding an ST type when contract is read only`, async () => {
         try {
-            await stm.setReadOnly(true, { from: accounts[0] });
-            await stm.addSecTokenType(`NEW_TYPE_NAME_${new Date().getTime()}`, CONST.settlementType.SPOT, CONST.nullFutureArgs, CONST.nullAddr, { from: accounts[0] });
+            await stmOwnedFacet.setReadOnly(true, { from: accounts[0] });
+            await stmStLedgerFacet.addSecTokenType(`NEW_TYPE_NAME_${new Date().getTime()}`, CONST.settlementType.SPOT, CONST.nullFutureArgs, CONST.nullAddr, { from: accounts[0] });
         } catch (ex) { 
             assert(ex.reason == 'Read-only', `unexpected: ${ex.reason}`);
-            await stm.setReadOnly(false, { from: accounts[0] });
+            await stmOwnedFacet.setReadOnly(false, { from: accounts[0] });
             return;
         }
-        await stm.setReadOnly(false, { from: accounts[0] });
+        await stmOwnedFacet.setReadOnly(false, { from: accounts[0] });
         assert.fail('expected contract exception');
     });
 
     it(`token types - should not allow adding a spot ST type with an expiry time`, async () => {
         try {
-            await stm.addSecTokenType(`NEW_TYPE_NAME_${new Date().getTime()}`, CONST.settlementType.SPOT, { ...CONST.nullFutureArgs, 
+            await stmStLedgerFacet.addSecTokenType(`NEW_TYPE_NAME_${new Date().getTime()}`, CONST.settlementType.SPOT, { ...CONST.nullFutureArgs, 
                 expiryTimestamp: new Date().getTime(),
                 underlyerTypeId: 0,
                        refCcyId: 0
@@ -129,7 +164,7 @@ contract("StMaster", accounts => {
 
     it(`token types - should not allow adding a spot ST type with an underlyer`, async () => {
         try {
-            await stm.addSecTokenType(`NEW_TYPE_NAME_${new Date().getTime()}`, CONST.settlementType.SPOT, { ...CONST.nullFutureArgs, 
+            await stmStLedgerFacet.addSecTokenType(`NEW_TYPE_NAME_${new Date().getTime()}`, CONST.settlementType.SPOT, { ...CONST.nullFutureArgs, 
                 expiryTimestamp: 0,
                 underlyerTypeId: CONST.tokenType.TOK_T1,
                        refCcyId: 0
@@ -142,9 +177,9 @@ contract("StMaster", accounts => {
     });
 
     it(`token types - should not allow adding a spot ST type with a reference currency`, async () => {
-        const spotTypes = (await stm.getSecTokenTypes()).tokenTypes.filter(p => p.settlementType == CONST.settlementType.SPOT);
+        const spotTypes = (await stmStLedgerFacet.getSecTokenTypes()).tokenTypes.filter(p => p.settlementType == CONST.settlementType.SPOT);
         try {
-            await stm.addSecTokenType(`NEW_TYPE_NAME_${new Date().getTime()}`, CONST.settlementType.SPOT, { ...CONST.nullFutureArgs, 
+            await stmStLedgerFacet.addSecTokenType(`NEW_TYPE_NAME_${new Date().getTime()}`, CONST.settlementType.SPOT, { ...CONST.nullFutureArgs, 
                 expiryTimestamp: 0,
                 underlyerTypeId: 0,
                        refCcyId: CONST.ccyType.USD, 
